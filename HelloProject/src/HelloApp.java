844cafab92467683e08a9325ddbdbd7fbf008bcf
Brianna Orr <b.orr@unb.ca>
2021-11-20 14:30:21 -0400

    Added a line to greeting.

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		//Message
-		System.out.println("Hello world!");
+		System.out.println("Hello world! How's it going?");
 	}
 
 }
