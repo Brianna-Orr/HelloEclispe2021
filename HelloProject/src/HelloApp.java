f91ccea9bebbddab7461ebf24bcf43025d8323a9
Brianna Orr <b.orr@unb.ca>
2021-11-21 18:32:52 -0400

    Changed my nickname to my full name.

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		//Message
-		System.out.println("Hello world! How's it going?");
+		System.out.println("Hello Brianna Orr!");
 	}
 
 }
